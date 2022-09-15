790fdadc093f575df43e4d9af0b96267830c5eb1
rock1357 <riccardosciarra88@gmail.com>
2022-09-15 15:52:23 +0200

    In the last version was introduceed the parameter_setting_step.py function. It allows for: -The possibility to use the noise standard deviation as parameter able to set the minimum height for the peak extraction -A query about the user wish if to scan the positive or negative part of the time trace. This implemantation is called,as a function, by the trace_portion_to_scan.py step

---
+++ parameter_setting_step.py
@@ -0,0 +1,64 @@
+import matplotlib.pyplot as plt
+
+def scanning(t,V,tn,Vn):
+
+#gain=input('did you use an amplification? Set the gain used in the experiment, although write 1:')
+#gain2=input('Write the gain used for the noise acquisition:')  
+gain=10^9;
+gain2=10^10;
+  
+'plot of the total time trace-figure divided by the gain'
+# plotting the points 
+plt.plot(t,V/gain)
+  
+# naming the x axis
+plt.xlabel('time[s]')
+# naming the y axis
+plt.ylabel('Current [A]')
+  
+# giving a title to my graph
+plt.title('total time trace')
+  
+# function to show the plot
+plt.show()
+
+V=V/gain2;
+V=V[n1:n2]/gain;
+t=t[n1:n2];
+k_factor=max(Vn)/std(Vn);
+noise=k_factor*std(Vn)
+print('the max/min value of the noise is bigger than the standard deviation value of about:',k_factor)
+#k_factor=input('choose the multiplication factor for the peak extraction above the std value',k_factor)
+
+
+'plot of the choosen partial time trace-figure divided by the gain'
+# plotting the points 
+plt.plot(t,V/gain)
+  
+# naming the x axis
+plt.xlabel('time[s]')
+# naming the y axis
+plt.ylabel('Current [A]')
+  
+# giving a title to my graph
+plt.title('your choosen partial time trace')
+  
+# function to show the plot
+plt.show()
+
+
+choice=input('do you have peaks on negative or positive side of the trace?Select negative [n] or positive [p]')
+if choice=='n':
+   'call negative scanning function'
+    negative_scanning(choice)
+else:
+   'call positive scanning function'
+    positive_scanning(choice)
+
+
+
+
+
+
+
+
