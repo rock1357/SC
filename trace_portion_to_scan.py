b5ebaf08ac0f9b4807a8dbb6cf703136b0d00d57
rock1357 <riccardosciarra88@gmail.com>
2022-09-17 13:19:31 +0200

    a reordering of the variable was made. gain and gain2 are queried in the first method while the k_factor will be asked in the third step: when the choice of a negative or positive analysis will be performed by the user.

--- trace_portion_to_scan.py
+++ trace_portion_to_scan.py
@@ -1,19 +1,25 @@
-
 #importing data from a csv file 
-'importing data from a csv file'
 import numpy as np
+import parameter_setting_step as pss
+
 c=10
+print('import the data of your recorded chronoamperometric experiment')
 #c=input('write the number of rows of your file data from which the numerical data starts from:')
 data = np.genfromtxt("electrode2-gain10-Fs10k-nofilter-bias-500mV-dark.csv", delimiter=",",skip_header=c)
+
+print('import the data of your recorded noise (no bias)')
+data2 = np.genfromtxt("electrode2-gain10-Fs10k-nofilter-bias-500mV-dark.csv", delimiter=",",skip_header=c)
 
 t= data[:,0]
 V= data[:,1]
 
+tn= data2[:,0]
+Vn= data2[:,1]
 
-#print(V,t)
+std=np.std(Vn)
 
 
-'''Once introduced the amperometric recording with name t and V here we can make the user to 
+'''Once introduced the chronoamperometric recording with name t and V here we can make the user to 
 choose the range of data to be scanned '''
 
 
@@ -21,7 +27,7 @@ choose the range of data to be scanned '''
 
 T=(t[1]-t[0]); 'inverse of the sampling frequency'
 Fs=(t[1]-t[0])**-1;'sampling frequency'
-print("you used a sampling frequency:",Fs)
+print("you used a sampling frequency:",round(Fs))
 
 N=len(t); 'number of points of the recording' 
 print('number of points of the trace:',N)
@@ -33,12 +39,14 @@ print('the total duration of the trace is',duration)
 
 #[time_interval1,time_interval2]=[input('enter the initial time you want the scanner to start:t_i='),input('enter the final time you want the scanner to finish:t_i=')]
 time_interval1= 1;
-time_interval2= 100;
+time_interval2= 50;
 
 n1=time_interval1/T
 n2=time_interval2/T
 
 
+"""the last point of the voltage must be positive to avoid an incomplete extraction of the 
+negative peak"""
 
 for s in range(round(n1),round(n2)):
     if s>n2-100:
@@ -47,5 +55,12 @@ for s in range(round(n1),round(n2)):
             break 
     break 
     break
+#gain=input('did you use an amplification? Set the gain used in the experiment, although write 1:')
+#gain2=input('Write the gain used for the noise acquisition:')  
+gain=10**10;
+    
+gain2=10**10;
+
+a=pss.parameter_setter(t,V,tn,Vn,round(n1),round(n2),std,gain,gain2)
 
 
